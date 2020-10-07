a78d48df16f6a5b7845fb3b382e0e3bfbd9f6b9a
andretwx <andretai.wx@gmail.com>
2020-10-07 15:51:08 +0800

    Added middleware to administrative routes.

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!Auth::check()) {
+            abort(403, "Unauthorized action!");
+        }
+        if(!Auth::user()->is_admin) {
+            abort(403, "Unauthorized action!");
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CoursesController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MsController;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,19 +27,19 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 // Management System
 
-Route::get('/ms', [MsController::class, 'index'])->name('ms-home');
-Route::get('/ms/statistics', function() { return view('ms.pages.stat'); })->name('ms-stats');
-Route::get('/ms/settings', function() { return view('ms.pages.setting'); })->name('ms-sets');
+Route::get('/ms', [MsController::class, 'index'])->name('ms-home')->middleware(IsAdmin::class);
+Route::get('/ms/statistics', function() { return view('ms.pages.stat'); })->name('ms-stats')->middleware(IsAdmin::class);
+Route::get('/ms/settings', function() { return view('ms.pages.setting'); })->name('ms-sets')->middleware(IsAdmin::class);
 
-Route::get('/ms/courses', [MsController::class, 'indexCourse'])->name('ms-course');
-Route::get('/ms/platforms', function() { return view('ms.pages.platform'); })->name('ms-platform');
-Route::get('/ms/promos', function() { return view('ms.pages.promo'); })->name('ms-promo');
-Route::get('/ms/users', function() { return view('ms.pages.user'); })->name('ms-user');
+Route::get('/ms/courses', [MsController::class, 'indexCourse'])->name('ms-course')->middleware(IsAdmin::class);
+Route::get('/ms/platforms', function() { return view('ms.pages.platform'); })->name('ms-platform')->middleware(IsAdmin::class);
+Route::get('/ms/promos', function() { return view('ms.pages.promo'); })->name('ms-promo')->middleware(IsAdmin::class);
+Route::get('/ms/users', function() { return view('ms.pages.user'); })->name('ms-user')->middleware(IsAdmin::class);
 
-Route::get('/ms/add', [MsController::class, 'add'])->name('ms-add');
-Route::get('/ms/edit', [MsController::class, 'edit'])->name('ms-edit');
-Route::get('/ms/remove', [MsController::class, 'remove'])->name('ms-remove');
+Route::get('/ms/add', [MsController::class, 'add'])->name('ms-add')->middleware(IsAdmin::class);
+Route::get('/ms/edit', [MsController::class, 'edit'])->name('ms-edit')->middleware(IsAdmin::class);
+Route::get('/ms/remove', [MsController::class, 'remove'])->name('ms-remove')->middleware(IsAdmin::class);
 
-Route::post('/ms/courses/store', [CoursesController::class, 'store'])->name('courses.store');
-Route::post('/ms/courses/update', [CoursesController::class, 'update'])->name('courses.update');
-Route::get('/ms/courses/delete', [CoursesController::class, 'delete'])->name('courses.delete');
+Route::post('/ms/courses/store', [CoursesController::class, 'store'])->name('courses.store')->middleware(IsAdmin::class);
+Route::post('/ms/courses/update', [CoursesController::class, 'update'])->name('courses.update')->middleware(IsAdmin::class);
+Route::get('/ms/courses/delete', [CoursesController::class, 'delete'])->name('courses.delete')->middleware(IsAdmin::class);
