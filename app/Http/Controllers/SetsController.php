a7226b584906e02764cc648fa0ff131e54528243
andretwx <andretai.wx@gmail.com>
2020-10-16 00:48:03 +0800

    Fixed issue (small).

--- app/Http/Controllers/SetsController.php
+++ app/Http/Controllers/SetsController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use Illuminate\Http\Request;
 use App\Models\Rating;
+use App\Models\User;
 
 class SetsController extends Controller
 {
     public function index() {
         return view('ms.pages.setting', [
             'ratings' => Rating::all(),
-            'numberOfUsers' => sizeof(Course::all()) 
+            'numberOfUsers' => sizeof(User::all())
         ]);
     }
 }
