a141a2a1faace8e826191d684c56294e7ce0d31f
Puong Huei <hueilau@users.noreply.github.com>
2020-12-02 13:09:37 +0800

    update home page with rec
    add collaFilter in recommend folder

---
+++ app/Http/Controllers/Recommend/VectorUser.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Recommend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Models\Course;
+
+class VectorUser extends Controller
+{
+    public function getAlphaUserFavouritedCourseIds($alphaUser)
+    {
+        $alphaFavs = DB::table('favourites')->select('course_id')->where('user_id', $alphaUser)->get()->toArray();
+        $alphaUserFavouritedCourseIds = [];
+        foreach($alphaFavs as $alphaFav) {
+            array_push($alphaUserFavouritedCourseIds, $alphaFav->course_id);
+        }
+        return $alphaUserFavouritedCourseIds;
+    }
+
+    public function getNonAlphaUsersIds($alphaUser)
+    {
+        $nonAlphaUsers = DB::table('favourites')->select('user_id')->where('user_id', '!=', $alphaUser)->distinct()->get()->toArray();
+        $nonAlphaUsersIds = [];
+        foreach($nonAlphaUsers as $nonAlphaUser) {
+            array_push($nonAlphaUsersIds, $nonAlphaUser->user_id);
+        }
+        return $nonAlphaUsersIds;
+    }
+
+    /**
+     * This method aims to calculate the vector scores between the alpha user and non-alpha users.
+     * Higher vector score means greater similarity between the alpha user and a non-alpha user in terms of favourites.
+     * In simpler terms, user A and user B both rated on the same few courses.
+     * However, user A and user B each has favourited different number of courses, for example, user A favourited 20 courses while user B favourited 100 courses.
+     * This means user B may be more generous in favoriting courses, making it a less significant factor.
+     * Hence, the proportions of favourites also carries weight, as you will notice in the calculation.
+     */
+    public function calc_vector($alphaUser)
+    {
+        # How to Calculate Vector
+        # Vector score = ( X1 * X1 / X2 ) * ( X1 * X1 / X3 )
+        # Where
+        #       X1 = Number of courses favourited by both alpha and non-alpha users
+        #       X2 = Total number of favourites by alpha user
+        #       X3 = Total number of favourites by non-alpha user
+
+        $alphaFavs = $this->getAlphaUserFavouritedCourseIds($alphaUser);
+        $nonAlphaUsers = $this->getNonAlphaUsersIds($alphaUser);
+        $vector_scores = [];
+        # Iterate through non-alpha users to calculate each vector score with alpha user
+        foreach($nonAlphaUsers as $nonAlphaUser) {
+            # Get all courses favourited by this user
+            $favouritedCourses = DB::table('favourites')->select('course_id')
+                                                      ->where('user_id', $nonAlphaUser)
+                                                      ->distinct()
+                                                      ->get()
+                                                      ->toArray();
+            $count = 0;
+            # Iterate through courses favourited by the alpha users and find any identical courses favourited by the non-alpha user.
+            foreach($alphaFavs as $alphaFav) {
+                foreach($favouritedCourses as $favouritedCourse) {
+                    if($alphaFav === $favouritedCourse->course_id) {
+                        $count++;
+                    }
+                }
+            }
+            # With the values we needed we calculate the vector score for a user in each iteration (refer to calculation).
+            $alpha = $count * ( $count / sizeof($alphaFavs) );
+            $nonAlpha = $count * ( $count / sizeof($favouritedCourses) );
+            $vector_score = $alpha * $nonAlpha;
+            $vector_scores[$nonAlphaUser] = $vector_score;
+        }
+        arsort($vector_scores);
+        $result = [];
+        # Because the array vector_scores is an associative array (does not have keys)
+        # We will need to create an array of keys from the associative array so that we can loop through it
+        $keys = array_keys($vector_scores);
+        for ($i=0; $i < 5 ; $i++) { 
+            # Because we have they key, we can use it to get elements from the vector_scores' associative array
+            $array = array('user_id' => $keys[$i], 'vector_score' => $vector_scores[$keys[$i]]);
+            array_push($result, $array);
+        }
+        return [$alphaUser, $result];
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,33 +46,32 @@
         </div>
     </div>
     <div>
-
         <div class="p-2 pt-6 block tracking-wide text-3xl font-bold">
-            Highest Rating Course
+            People are viewing
         </div>
         <div class="flex">
-            @foreach($courses->ratings as $course)
-            <a class="mt-2 px-2 w-64" href="/course/{{$course->course->id}}">
+            @foreach($courses->ratingRec as $course)
+            <a class="mt-2 px-2 w-64" href="/course/{{$course->id}}">
                 <div class="bg-white border rounded-lg overflow-hidden hover:shadow-xl">
-                    <img class="h-40 w-full object-cover" src="{{$course->course->image}}" alt="{{$course->course->description}}">
+                    <img class="h-40 w-full object-cover" src="{{$course->image}}" alt="{{$course->description}}">
                     <div class="p-6">
                         <div class="flex items-baseline">
-                            <span class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">{{$course->course->platform->platform}}</span>
+                            <span class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">{{$course->platform->platform}}</span>
                             <div class="ml-2 text-gray-600 text-xs uppercase font-semibold truncate">
-                                {{$course->course->category->category}}
+                                {{$course->category->category}}
                             </div>
                         </div>
 
-                        <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ $course->course->title }}</h4>
+                        <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ $course->title }}</h4>
 
                         <div class="flex mt-1">
                             <div class="text-gray-600 capitalize w-24 font-semibold truncate">
-                                {{$course->course->instructor}}</div>
-                            <div class="text-orange-600 ml-3">{{$course->course->avgRating()}} <i class="fas fa-star fa-sm pl-1"></i></div>
+                                {{$course->instructor}}</div>
+                            <div class="text-orange-600 ml-3">{{$course->avgRating()}} <i class="fas fa-star fa-sm pl-1"></i></div>
                         </div>
                         <div class="flex items-baseline">
                             <div class="mt-2 text-red-700 text-xl font-semibold tracking-wide">
-                                {{ $course->course->price }}
+                                {{ $course->price }}
                             </div>
                         </div>
                     </div>
@@ -82,6 +81,43 @@
 
         </div>
     </div>
+    <div>
+        <div class="p-2 pt-6 block tracking-wide text-3xl font-bold">
+            People added in their lists
+        </div>
+        <div class="flex">
+            @foreach($courses->favRec as $course)
+            <a class="mt-2 px-2 w-64" href="/course/{{$course->id}}">
+                <div class="bg-white border rounded-lg overflow-hidden hover:shadow-xl">
+                    <img class="h-40 w-full object-cover" src="{{$course->image}}" alt="{{$course->description}}">
+                    <div class="p-6">
+                        <div class="flex items-baseline">
+                            <span class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">{{$course->platform->platform}}</span>
+                            <div class="ml-2 text-gray-600 text-xs uppercase font-semibold truncate">
+                                {{$course->category->category}}
+                            </div>
+                        </div>
+
+                        <h4 class="mt-1 font-semibold text-lg leading-tight truncate">{{ $course->title }}</h4>
+
+                        <div class="flex mt-1">
+                            <div class="text-gray-600 capitalize w-24 font-semibold truncate">
+                                {{$course->instructor}}</div>
+                            <div class="text-orange-600 ml-3">{{$course->avgRating()}} <i class="fas fa-star fa-sm pl-1"></i></div>
+                        </div>
+                        <div class="flex items-baseline">
+                            <div class="mt-2 text-red-700 text-xl font-semibold tracking-wide">
+                                {{ $course->price }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            @endforeach
+
+        </div>
+    </div>
+    
     <div>
         <div class="p-2 pt-6 block tracking-wide text-3xl font-bold">
             Top Categories
